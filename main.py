726278066417ac4a4d487c6d2ea772f76ac28256
JameDino22 <jamedino73@gmail.com>
2025-04-08 17:04:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from playwright.sync_api import sync_playwright
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains
 
 def extract_emails_from_url(url):
     with sync_playwright() as p:
